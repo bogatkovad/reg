ba868e592cc171891e28b6f3df074c832c9f8906
bogatkovad <ardaffler@ya.ru>
2021-12-25 19:15:12 +0300

    first commit

---
+++ db.php
@@ -0,0 +1,8 @@
+<?php
+$con = mysqli_connect("localhost","root","15022004","register");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+?>
